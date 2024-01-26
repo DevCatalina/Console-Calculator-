a63dc209d565e7e4925066db4ea874e6b7adf551
Catalina Calero <130515858+DevCatalina@users.noreply.github.com>
2024-01-26 17:13:29 +0000

    ConsoleCalc.cs
    the C# Document, it worked in college so it should work now

---
+++ ConsoleCalc.cs
@@ -0,0 +1,85 @@
+static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for + and 2 for -");
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            if (userChoice == 1)
+            {
+                Console.WriteLine(NumbersAdd());
+            }
+
+            else if (userChoice == 2)
+            {
+                Console.WriteLine(NumberSub());
+            }
+
+            else if (userChoice == 3)
+            {
+                Console.WriteLine(NumberDivide());
+            }
+
+            else if (userChoice == 4)
+            {
+                Console.WriteLine(NumberTimes());
+            }
+        }
+
+        static double NumbersAdd()
+        {
+
+            Console.WriteLine("Enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double answer = num1 + num2;
+
+            return answer;
+
+        }
+
+        static double NumberSub()
+        {
+            Console.WriteLine("Enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double answer = num1 - num2;
+
+            return answer;
+        }
+
+        static double NumberDivide()
+        {
+            Console.WriteLine("Enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double answer = num1 / num2;
+
+            return answer;
+        }
+
+        static double NumberTimes()
+        {
+            Console.WriteLine("Enter first number");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double answer = num1 * num2;
+
+            return answer;
+        }
+
+    
+
+
+ 
+    
+ 
